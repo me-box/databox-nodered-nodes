986bcb46e668a184f6dc69f6b19f4dc1bf8f847e
tlodge <tlodge@gmail.com>
2016-10-13 16:55:48 +0100

    cp commit

---
+++ 89-twitter.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2016 Tom Lodge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+
+// If you use this as a template, update the copyright with your own name.
+module.exports = function(RED) {
+
+    "use strict";
+    var request = require('request');
+    var moment = require('moment');
+    
+    function Twitter(n) {
+    
+        const API_ENDPOINT 	= process.env.TESTING ? {} : JSON.parse(process.env[`DATASOURCE_${n.id}`]);
+        const API_URL 		= process.env.TESTING ? `${process.env.MOCK_DATA_SOURCE}/reading/latest` : `http://${API_ENDPOINT.hostname}${API_ENDPOINT.api_url}/data/latest`;
+        const SENSOR_ID 	= process.env.TESTING ? n.subtype : API_ENDPOINT.sensor_id;
+
+        this.name = n.name;
+
+        RED.nodes.createNode(this,n);
+        var node = this;
+       
+        
+		const options = {
+  			method: 'post',
+  			body: {sensor_id: SENSOR_ID},
+  			json: true,
+  			url: API_URL,
+		}
+		
+		console.log(options);
+		
+		const periodic = setInterval(function(){
+									request(options, function (err, res, body) {
+										if (err) {
+											console.log(err, 'error posting json')
+										}else{
+											if (body.length > 0){
+												const result = body[0];
+												if (result.length > 0){
+													const {time,value} = result[0];
+													console.log("gor result");
+													console.log(value);
+													
+													console.log("sending");
+													
+													console.log({
+															name: n.name || "twitter",
+															id:  n.id,
+															type: "twitter",
+															payload: {
+																ts: moment.utc(time).unix(),
+																value: value, 
+															},
+													});
+													
+													node.send({
+															name: n.name || "twitter",
+															id:  n.id,
+															type: "twitter",
+															payload: {
+																ts: moment.utc(time).unix(),
+																value: value, 
+															},
+													});   
+												}
+											}	
+										}
+									});
+						}, 10000);
+
+        this.on("close", function() {
+        	console.log(`${node.id} stopping requests`);
+			clearInterval(periodic);
+        });
+    }
+
+    // Register the node by name. This must be called beforeoverriding any of the
+    // Node functions.
+    RED.nodes.registerType("twitter",Twitter);
+
+}
