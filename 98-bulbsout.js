46876f03eaa7d510bf5627c63c42069dcf506e89
tlodge <tlodge@gmail.com>
2016-10-24 16:48:32 +0100

    cp commit

--- 98-bulbsout.js
+++ 98-bulbsout.js
@@ -61,7 +61,7 @@ module.exports = function(RED) {
 			
         	const options = {
   				method: 'post',
-  				body: {actuator_id: SENSOR_ID, data: msg.payload ? Number(msg.payload) : n.value ? Number(n.value) : 0 : 0},
+  				body: {actuator_id: SENSOR_ID, data: msg.payload ? Number(msg.payload) : n.value ? Number(n.value) : 0},
   				json: true,
   				url: API_URL,
 			}
