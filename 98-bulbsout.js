c19580972f2b5e3cf6a8aff1ba1a0e6c59292946
tlodge <tlodge@gmail.com>
2016-10-24 17:20:03 +0100

    cp commit

--- 98-bulbsout.js
+++ 98-bulbsout.js
@@ -21,29 +21,6 @@ module.exports = function(RED) {
    
     function Bulbs(n) {
     
- 		/**
- * Copyright 2016 Tom Lodge
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
-// If you use this as a template, update the copyright with your own name.
-module.exports = function(RED) {
-    
-    "use strict";
-   
-    function Bulbs(n) {
-    
  		console.log(process.env);
  		const API_ENDPOINT 	= process.env.TESTING ? {} : JSON.parse(process.env[`DATASOURCE_${n.id}`]);
         const API_URL 		= process.env.TESTING ? `${process.env.MOCK_DATA_SOURCE}/actuate` : `http://${API_ENDPOINT.hostname}${API_ENDPOINT.api_url}/actuate`;
@@ -55,6 +32,25 @@ module.exports = function(RED) {
         var node = this;
        
 		this.on('input', function (msg) {
+			console.log("bulbs out seen input");
+			console.log(msg);
+			
+			
+        	const options = {
+  				method: 'post',
+  				body: {actuator_id: SENSOR_ID, data: msg.payload ? Number(msg.payload) : n.value ? Number(n.value) : 0},
+  				json: true,
+  				url: API_URL,
+			}
+			console.log(options);
+			request(options, function (err, res, body) {
+						if (err) {
+							console.log(err, 'error posting json')
+						}else{
+							console.log("result is ");
+							console.log(body);
+						}
+			});	
 		
         });
         
