f6155ab5cfa21629678ba2d7134767810cf48cb9
tlodge <tlodge@gmail.com>
2016-10-27 13:03:55 +0100

    cp commit

--- 76-plugin.js
+++ 76-plugin.js
@@ -25,7 +25,7 @@ module.exports = function(RED) {
  		const API_ENDPOINT 	= process.env.TESTING ? {} : JSON.parse(process.env[`DATASOURCE_${n.id}`]);
         const API_URL 		= process.env.TESTING ? `${process.env.MOCK_DATA_SOURCE}/data/latest` : `http://${API_ENDPOINT.hostname}${API_ENDPOINT.api_url}/data/latest`;
         const SENSOR_ID 	= process.env.TESTING ? n.subtype : API_ENDPOINT.sensor_id;
-
+		let socket;
 		
         this.name = n.name;
 
