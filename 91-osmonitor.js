35855b9afd11e6752de77ac69611fb97dd4c758c
tlodge <tlodge@gmail.com>
2017-03-14 12:56:37 +0000

    update osmonitor to new version

--- 91-osmonitor.js
+++ 91-osmonitor.js
@@ -14,74 +14,93 @@
  * limitations under the License.
  **/
 
-
-// If you use this as a template, update the copyright with your own name.
 module.exports = function(RED) {
 
     "use strict";
     var request = require('request');
     var moment = require('moment');
-    
+    var databox = require('node-databox');
+    var url = require("url");
+
     function OSMonitor(n) {
     
-        const API_ENDPOINT 	= process.env.TESTING ? {} : JSON.parse(process.env[`DATASOURCE_${n.id}`]);
-        const API_URL 		= process.env.TESTING ? `${process.env.MOCK_DATA_SOURCE}/reading/latest` : `http://${API_ENDPOINT.hostname}${API_ENDPOINT.api_url}/reading/latest`;
-        const SENSOR_ID 	= process.env.TESTING ? n.subtype : API_ENDPOINT.sensor_id;
+        //const API_ENDPOINT 	= process.env.TESTING ? {} : JSON.parse(process.env[`DATASOURCE_${n.id}`]);
+        //const API_URL 		= process.env.TESTING ? `${process.env.MOCK_DATA_SOURCE}/reading/latest` : `http://${API_ENDPOINT.hostname}${API_ENDPOINT.api_url}/reading/latest`;
+        //const SENSOR_ID 	= process.env.TESTING ? n.subtype : API_ENDPOINT.sensor_id;
+
+
+        const  API_ENDPOINT = JSON.parse(process.env[`DATASOURCE_${subtype}`] || '{}');
+    	const  HREF_ENDPOINT = API_ENDPOINT.href || '';	
+        
+
+        console.log("HAVE API ENDPOINT: ");
+        console.log(API_ENDPOINT);
+        console.log("HREF ENDPOINT");
+        console.log(HREF_ENDPOINT);
 
         this.name = n.name;
 
         RED.nodes.createNode(this,n);
         var node = this;
-       
         
-		const options = {
-  			method: 'post',
-  			body: {sensor_id: SENSOR_ID},
-  			json: true,
-  			url: API_URL,
+        new Promise((resolve,reject)=>{
+    			setTimeout(resolve,10000);
+  		}).then(()=>{
+        	var dataEmitter = null; 
+        	
+        	if (HREF_ENDPOINT != ''){
+
+        		console.log("setting up endpoints...");
+        		var endpointUrl = url.parse(HREF_ENDPOINT);
+        		var dsID = API_ENDPOINT['item-metadata'].filter((itm)=>{return itm.rel === 'urn:X-databox:rels:hasDatasourceid'; })[0].val;
+        		var dsUrl = endpointUrl.protocol + '//' + endpointUrl.host;
+        		var dsType = API_ENDPOINT['item-metadata'].filter((itm)=>{return itm.rel === 'urn:X-databox:rels:hasType';})[0].val;
+        
+        		//pull out the latest....
+        		databox.timeseries.latest(dsUrl, dsID)
+		        .then((data)=>{
+		            console.log("GOT LATEST READING!");
+		            console.log(data);
+		            console.log(data[0].data);
+		        })
+		        .catch((err)=>{
+		            console.log("[Error getting timeseries.latest]",dsUrl, dsID);
+		        });
+
+		        //subscribe
+		        databox.subscriptions.connect(HREF_ENDPOINT)
+		        .then((emitter)=>{
+		        	dataEmitter = emitter;
+		        	var endpointUrl = url.parse(HREF_ENDPOINT);
+					var dsID = API_ENDPOINT['item-metadata'].filter((itm)=>{return itm.rel === 'urn:X-databox:rels:hasDatasourceid'; })[0].val;
+            		var dsUrl = endpointUrl.protocol + '//' + endpointUrl.host;
+            		console.log("[subscribing]",dsUrl,dsID);
+            		databox.subscriptions.subscribe(dsUrl,dsID,'ts').catch((err)=>{console.log("[ERROR subscribing]",err)});
+		        	
+		        	dataEmitter.on('data',(hostname, dsID, data)=>{
+		                console.log(hostname, dsID, data);
+		                node.send({
+							name: n.name || "osmonitor",
+							id:  n.id,
+							subtype: n.subtype,
+							type: "osmonitor",
+							payload: {
+								ts: moment.utc(time).unix(),
+								value: data, 
+							},
+						});   
+		            });
+
+		            dataEmitter.on('error',(error)=>{
+		                console.log(error);
+		            });
+		        
+		        }).catch((err)=>{console.log("[Error] connecting ws endpoint ",err);});
+			}
 		}
-		
-		
-		const periodic = setInterval(function(){
-									request(options, function (err, res, body) {
-										if (err) {
-											console.log(err, 'error posting json')
-										}else{
-											if (body.length > 0){
-												const result = body[0];
-												if (result.length > 0){
-													const {time,value} = result[0];
-													
-													console.log({
-															name: n.name || "osmonitor",
-															id:  n.id,
-															subtype: n.subtype,
-															type: "osmonitor",
-															payload: {
-																ts: moment.utc(time).unix(),
-																value: Number(value), 
-															},
-													});
-													
-													node.send({
-															name: n.name || "osmonitor",
-															id:  n.id,
-															subtype: n.subtype,
-															type: "osmonitor",
-															payload: {
-																ts: moment.utc(time).unix(),
-																value: Number(value), 
-															},
-													});   
-												}
-											}	
-										}
-									});
-						}, 3000);
 
         this.on("close", function() {
         	console.log(`${node.id} stopping requests`);
-			clearInterval(periodic);
         });
     }
 
