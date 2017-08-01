ff0c913c977be122c9d72d50be5ed65386f26926
tlodge <tlodge@gmail.com>
2017-08-01 21:14:00 +0100

    adding openface processor

--- openface/12-openface.js
+++ openface/12-openface.js
@@ -2,28 +2,35 @@ module.exports = function(RED) {
     
     "use strict";
     var WebSocket = require('ws');
-    
+    var socket;
+
     function OpenFace(n) {
         
         RED.nodes.createNode(this,n);
         var node = this;
 
-        var socket = new WebSocket("ws:127.0.0.1:9001");
-        socket.binaryType = "arraybuffer";
+        try{
+            socket = new WebSocket("ws:openface:9001");
+            socket.binaryType = "arraybuffer";
 
-        socket.onopen = function() {
-           console.log("created socket");
-        }
+            socket.onopen = function() {
+                console.log("created socket");
+            }
         
-        socket.onmessage = function(e) {
-            console.log("got message",e);
-            node.send(e);
+            socket.onmessage = function(e) {
+                console.log("got message",e);
+                node.send(e);
+            }
+        }catch(error){
+            console.log("error connecting to socket", error);
         }
 
         this.on('input', function (msg) {
             console.log("seen an image, sending to openface");
             if (msg.dataURL){
-               socket.send(msg);
+                if (socket){
+                    socket.send(msg);
+                }
             }
         });
     }
