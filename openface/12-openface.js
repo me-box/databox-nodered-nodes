5edde606463d43457350acdb620728586fddc82c
tlodge <tlodge@gmail.com>
2017-08-02 11:25:26 +0100

    debugging websockets

--- openface/12-openface.js
+++ openface/12-openface.js
@@ -1,8 +1,11 @@
 module.exports = function(RED) {
     
     "use strict";
-    var WebSocket = require('ws');
-    var socket;
+    //var WebSocket = require('ws');
+    //var socket;
+    var net = require('net');
+     
+
 
     function OpenFace(n) {
         console.log("in openfacce...");
@@ -10,7 +13,26 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         var node = this;
 
-        try{
+        var client = new net.Socket();
+        var connected = false;
+
+        client.on("error", function(err){
+            connected = false;
+            console.log("error connecting, retrying in 2 sec");
+            setTimeout(function(){connect()}, 2000);
+        });
+
+        client.on("message", function(msg){
+            console.log("nice - seen message!!", msg);
+        });
+        
+        client.on('uncaughtException', function (err) {
+            connected = false;
+            console.error(err.stack);
+            setTimeout(function(){connect()}, 2000);
+        });
+
+        /*try{
             console.log("creating new websocket");
 
             socket = new WebSocket("ws:openface:9001");
@@ -29,25 +51,26 @@ module.exports = function(RED) {
             socket.onclose = function(event){
                 console.log("closed socket", event);
             }
-            
+
             socket.onmessage = function(e) {
                 console.log("got message",e);
                 node.send("hello");
             }
         }catch(error){
             console.log("error connecting to socket", error);
-        }
+        }*/
 
         this.on('input', function (msg) {
             console.log("seen an image, sending to openface");
             if (msg.dataURL){
-                if (socket){
+                client.write(JSON.stringify(msg));
+                /*if (socket){
                     try{
                         socket.send("ahello!!");
                     }catch(error){
                         console.log("error sending", error)
                     }
-                }
+                }*/
             }
         });
     }
