d91626ec4574daf8aa21897f503f69d466d7d419
tlodge <tlodge@gmail.com>
2017-08-02 10:30:13 +0100

    debugging websockets

--- openface/12-openface.js
+++ openface/12-openface.js
@@ -5,18 +5,31 @@ module.exports = function(RED) {
     var socket;
 
     function OpenFace(n) {
-        
+        console.log("in openfacce...");
+
         RED.nodes.createNode(this,n);
         var node = this;
 
         try{
+            console.log("creating new websocket");
+
             socket = new WebSocket("ws:openface:9001");
             socket.binaryType = "arraybuffer";
+
+            console.log("done creating new websocket");
+
+            socket.onerror = function(err){
+                console.log("errored creating scoket", err);
+            }
 
             socket.onopen = function() {
                 console.log("created socket");
             }
         
+            socket.onclose = function(event){
+                console.log("closed socket", event);
+            }
+            
             socket.onmessage = function(e) {
                 console.log("got message",e);
                 node.send("hello");
