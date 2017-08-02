385edc2808ccfc505c9acae7d4510d97a5e4c4bd
tlodge <tlodge@gmail.com>
2017-08-02 17:44:29 +0100

    use netstring proto to send to openface

--- openface/12-openface.js
+++ openface/12-openface.js
@@ -7,6 +7,43 @@ module.exports = function(RED) {
     var client = new net.Socket();
     var connected = false;
     
+
+    const NETSTRING_DELIMITER = ',';
+    const NETSTRING_SEPARATOR = ':';
+    
+    const netstringify = (string, { encoding = 'utf-8', response = 'string' } = {}) => {
+
+        let result = [];
+        let input = [];
+
+        if (!input) {
+            return;
+        }
+
+        if (!Array.isArray(string)) {
+            input.push(string);
+        } else {
+            input = string;
+        }
+
+        input.forEach((text) => {
+            let netstring = [];
+            netstring.push(new Buffer(`${text.length}${NETSTRING_SEPARATOR}`, encoding));
+            netstring.push(new Buffer(text, encoding));
+            netstring.push(new Buffer(NETSTRING_DELIMITER, encoding));
+            netstring = Buffer.concat(netstring);
+            result.push(netstring);
+        });
+        //For string result
+        if (result.length > 0 && response === 'string') {
+            result = result.map(netstring => netstring.toString(encoding));
+            return result.join('');
+        }
+        //Return as buffer for all the other types.
+        return Buffer.concat(result);
+    };
+
+
     function connect(fn){
         connected = false;
    
@@ -76,8 +113,8 @@ module.exports = function(RED) {
         this.on('input', function (msg) {
             console.log("seen something, sending to openface");
             //if (msg.dataURL){
-            const data = JSON.stringify(msg);
-            client.write(`${data.length}:${data}`);
+            const data = JSON.stringify({value:"hello"});
+            client.write(netstringify(data));
                 /*if (socket){
                     try{
                         socket.send("ahello!!");
