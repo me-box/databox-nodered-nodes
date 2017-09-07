749bbe1f806bfd5aa13abecf44f14eb22b1d1a58
tlodge <tlodge@gmail.com>
2017-09-07 12:26:19 +0100

    added netstring to lib

--- openface/12-openface.js
+++ openface/12-openface.js
@@ -5,70 +5,7 @@ module.exports = function(RED) {
     //var socket;
     var net = require('net');
     var connected = false;
-    
-
-    const NETSTRING_DELIMITER = ',';
-    const NETSTRING_SEPARATOR = ':';
-    const NETSTRING_SEPARATOR_CODE = 58;
-
-    const netstringify = (string, { encoding = 'utf-8', response = 'string' } = {}) => {
-
-        let result = [];
-        let input = [];
-
-        if (!input) {
-            return;
-        }
-
-        if (!Array.isArray(string)) {
-            input.push(string);
-        } else {
-            input = string;
-        }
-
-        input.forEach((text) => {
-            let netstring = [];
-            netstring.push(new Buffer(`${text.length}${NETSTRING_SEPARATOR}`, encoding));
-            netstring.push(new Buffer(text, encoding));
-            netstring.push(new Buffer(NETSTRING_DELIMITER, encoding));
-            netstring = Buffer.concat(netstring);
-            result.push(netstring);
-        });
-        //For string result
-        if (result.length > 0 && response === 'string') {
-            result = result.map(netstring => netstring.toString(encoding));
-            return result.join('');
-        }
-        //Return as buffer for all the other types.
-        return Buffer.concat(result);
-    };
-
-    const parse = (netstring, { encoding = 'utf-8', response = 'string' } = {}) => {
-        let result = [];
-        if (!netstring) {
-            return;
-        }
-        netstring = new Buffer(netstring, encoding);
-
-        for (let i = 0, val, lenStart = 0, len, stringStart, string; i < netstring.length ; i++) {
-
-            val = netstring[i];
-            //First find the ':' - NETSTRING_SEPARATOR character.
-            if (NETSTRING_SEPARATOR_CODE === val) {
-                //Find the length digits on the left side of NETSTRING_SEPARATOR(:)
-                len = netstring.toString(encoding, lenStart, i);
-                len = Number.parseInt(len);
-                //Find the string based on the length value and slice the string.
-                stringStart = i + 1;
-                string = netstring.slice(stringStart, stringStart + len);
-                result.push((response === 'string') ? string.toString(encoding): string);
-                //Reset index to next string.
-                i = stringStart + len + 1;
-                lenStart = i;
-            }
-        }
-        return result;
-    };
+    var netstring = require("../utils/netstring");
 
     function connect(fn){
         connected = false;
@@ -100,7 +37,7 @@ module.exports = function(RED) {
 
             client.on("data", function(data){  
                 try{
-                    var msg = parse(data);
+                    var msg = netstring.parse(data);
                 
                     var parsed = msg.map(function(item){
                         return JSON.parse(item);
@@ -158,7 +95,7 @@ module.exports = function(RED) {
 
         this.on('input', function (msg) {
             const data = JSON.stringify(msg);
-            client.write(netstringify(data));
+            client.write(netstring.netstringify(data));
         });
 
         this.on('close', function(){
---
+++ utils/netstring.js
@@ -0,0 +1,67 @@
+module.exports = function() {
+
+    const NETSTRING_DELIMITER = ',';
+    const NETSTRING_SEPARATOR = ':';
+    const NETSTRING_SEPARATOR_CODE = 58;
+
+    const netstringify = (string, { encoding = 'utf-8', response = 'string' } = {}) => {
+
+        let result = [];
+        let input = [];
+
+        if (!input) {
+            return;
+        }
+
+        if (!Array.isArray(string)) {
+            input.push(string);
+        } else {
+            input = string;
+        }
+
+        input.forEach((text) => {
+            let netstring = [];
+            netstring.push(new Buffer(`${text.length}${NETSTRING_SEPARATOR}`, encoding));
+            netstring.push(new Buffer(text, encoding));
+            netstring.push(new Buffer(NETSTRING_DELIMITER, encoding));
+            netstring = Buffer.concat(netstring);
+            result.push(netstring);
+        });
+        //For string result
+        if (result.length > 0 && response === 'string') {
+            result = result.map(netstring => netstring.toString(encoding));
+            return result.join('');
+        }
+        //Return as buffer for all the other types.
+        return Buffer.concat(result);
+    };
+
+    const parse = (netstring, { encoding = 'utf-8', response = 'string' } = {}) => {
+
+        let result = [];
+        
+        if (!netstring) {
+            return;
+        }
+        netstring = new Buffer(netstring, encoding);
+
+        for (let i = 0, val, lenStart = 0, len, stringStart, string; i < netstring.length ; i++) {
+
+            val = netstring[i];
+            //First find the ':' - NETSTRING_SEPARATOR character.
+            if (NETSTRING_SEPARATOR_CODE === val) {
+                //Find the length digits on the left side of NETSTRING_SEPARATOR(:)
+                len = netstring.toString(encoding, lenStart, i);
+                len = Number.parseInt(len);
+                //Find the string based on the length value and slice the string.
+                stringStart = i + 1;
+                string = netstring.slice(stringStart, stringStart + len);
+                result.push((response === 'string') ? string.toString(encoding): string);
+                //Reset index to next string.
+                i = stringStart + len + 1;
+                lenStart = i;
+            }
+        }
+        return result;
+    };
+}
