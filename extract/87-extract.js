3cfbf4cd3fcb88b6d8fbe62b7b9f7c3b80c463c1
tlodge <tlodge@gmail.com>
2018-07-04 16:57:54 +0100

    added in objet assign to remember all vals of extract

--- extract/87-extract.js
+++ extract/87-extract.js
@@ -34,41 +34,18 @@ module.exports = function (RED) {
                 const extracted = paths.reduce((acc, path) => {
                     if (path.length > 0) {
                         const extracted = _extract(msg, path);
-                        console.log("extracted is", extracted);
 
                         if (extracted != undefined) {
                             var keys = [msg.id, ...path]
                             var value = extracted;
-                            var res = keys.reduceRight((value, key) => ({ [key]: value }), value);
-                            console.log(JSON.stringify(res, null, 4));
-                            return res;
-                            //const obj = [msg.id, ...path].reduce((o, i) => o[i], {})
-
-                            //console.log("have obj", JSON.stringify(obj));
-                            /*const obj = [msg.id, ...path].reverse().reduce((acc, key, i) => {
-                                if (i == 0) {
-                                    acc = {};
-
-                                    acc[key] = extracted;
-                                    console.log("acc is", JSON.stringify(acc));
-                                } else {
-                                   
-                                }
-                                return acc[key];
-                            }, acc);
-
-                            console.log("extracted", JSON.stringify(obj, null, 4));
-                            //[msg.id, ...path].reduce((o, i) => o[i], acc) = extracted;
-                            //console.log("extracted obj", JSON.stringify(acc, null, 4));
-                            //const ref = [msg.id, ...path].reduce((x, k) => x[k])
-                            //ref = extracted;*/
+                            return Object.assign({}, acc, keys.reduceRight((value, key) => ({ [key]: value }), extracted));
                             //acc[[msg.id, ...path].join(".")] = extracted;
                         }
                     }
                     return acc;
                 }, {});
 
-                console.log("----- extracted is ", JSON.stringify(extracted));
+                console.log("----- extracted is ", JSON.stringify(extracted, null, 4));
 
                 if (Object.keys(extracted).length > 0) {
                     node.send({
